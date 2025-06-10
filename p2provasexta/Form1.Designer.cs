2af3bc8f7931d23b82538c92c8f7a84830d09b6b
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-10 14:50:50 -0300

    frmLoguin pronto e funcional

--- p2provasexta/Form1.Designer.cs
+++ p2provasexta/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(327, 290);
+            button1.Location = new Point(323, 302);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
