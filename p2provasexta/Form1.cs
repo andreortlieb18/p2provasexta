8664d92e0fa02bbffd7fe2d5ce053613c8ca5022
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-10 14:49:24 -0300

    criei um arquivo csv e programei ele nas funções do button

--- p2provasexta/Form1.cs
+++ p2provasexta/Form1.cs
@@ -22,6 +22,44 @@ namespace p2provasexta
                 frmPrinc.ShowDialog();
                 return;
             }
-        }
+            string caminhoCSV = "usuario.csv";
+
+            if (File.Exists(caminhoCSV))
+            {
+                bool credencialValida = false;
+
+                using (StreamReader sr = new StreamReader(caminhoCSV))
+                {
+                    while (sr.EndOfStream)
+                    {
+                        string linha = sr.ReadLine();
+                        string[] dados = linha.Split(',');
+
+                        if (dados.Length == 2)
+                        {
+                            if (dados[0] == usuario && dados[1] == senha)
+                            {
+                                credencialValida = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (credencialValida)
+                {
+                    frmPrinc FrmPrinc = new frmPrinc();
+                    FrmPrinc.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Usuário ou senha inválidos.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                }
+            }
+            else
+            {
+                MessageBox.Show("Arquivo de usuários não encontrado.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }        
     }
 }
+
