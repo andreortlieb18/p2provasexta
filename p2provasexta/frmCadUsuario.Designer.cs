0d8908efc3b9da26f1789629e3fcc45392518f17
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-10 15:12:21 -0300

    Criei um form chamado frmCadUsuario

--- p2provasexta/frmCadUsuario.Designer.cs
+++ p2provasexta/frmCadUsuario.Designer.cs
@@ -29,25 +29,24 @@
         private void InitializeComponent()
         {
             colorDialog1 = new ColorDialog();
-            checkBox1 = new CheckBox();
+            label1 = new Label();
             SuspendLayout();
             // 
-            // checkBox1
+            // label1
             // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(463, 157);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(101, 24);
-            checkBox1.TabIndex = 0;
-            checkBox1.Text = "checkBox1";
-            checkBox1.UseVisualStyleBackColor = true;
+            label1.AutoSize = true;
+            label1.Location = new Point(287, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Cadastro de Usuário";
             // 
             // frmCadUsuario
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(checkBox1);
+            Controls.Add(label1);
             Name = "frmCadUsuario";
             Text = "frmCadUsuario";
             ResumeLayout(false);
@@ -57,6 +56,6 @@
         #endregion
 
         private ColorDialog colorDialog1;
-        private CheckBox checkBox1;
+        private Label label1;
     }
 }
