50293a4e1f7dc9b726e1d79bcc5fe7c208a7c860
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-10 13:59:01 -0300

    fiz o design do frmLoguin e adicionei 3 label, 2 textBox e 1 button

--- p2provasexta/Form1.Designer.cs
+++ p2provasexta/Form1.Designer.cs
@@ -28,22 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             textBox1 = new TextBox();
+            textBox2 = new TextBox();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(327, 290);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "ENTRAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(342, 96);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 20);
+            label1.TabIndex = 1;
+            label1.Text = "LOGUIN";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(344, 146);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Usuário";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(344, 213);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Senha";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(524, 151);
+            textBox1.Location = new Point(310, 169);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(125, 27);
-            textBox1.TabIndex = 0;
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(310, 236);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 5;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
             Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -52,6 +105,11 @@
 
         #endregion
 
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
         private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
