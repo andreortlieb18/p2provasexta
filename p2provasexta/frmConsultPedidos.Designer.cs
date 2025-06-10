a8d1243127e797e774b827d2c7b9906c77931a28
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-10 16:42:33 -0300

    adicionei ao frmConsultPedidos 6 label, 1 button e 2 listView

--- p2provasexta/frmConsultPedidos.Designer.cs
+++ p2provasexta/frmConsultPedidos.Designer.cs
@@ -31,6 +31,15 @@
             label1 = new Label();
             textBox1 = new TextBox();
             label2 = new Label();
+            button1 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            listView1 = new ListView();
+            listView2 = new ListView();
             SuspendLayout();
             // 
             // label1
@@ -38,13 +47,13 @@
             label1.AutoSize = true;
             label1.Location = new Point(37, 78);
             label1.Name = "label1";
-            label1.Size = new Size(264, 20);
+            label1.Size = new Size(36, 20);
             label1.TabIndex = 0;
-            label1.Text = "Digite aqui o cpf que deseja consultar:";
+            label1.Text = "CPF:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(37, 111);
+            textBox1.Location = new Point(79, 71);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(125, 27);
             textBox1.TabIndex = 1;
@@ -58,11 +67,99 @@
             label2.TabIndex = 2;
             label2.Text = "Consulta de Pedidos";
             // 
+            // button1
+            // 
+            button1.Location = new Point(371, 137);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(390, 202);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(398, 210);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 20);
+            label4.TabIndex = 5;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(406, 218);
+            label5.Name = "label5";
+            label5.Size = new Size(50, 20);
+            label5.TabIndex = 6;
+            label5.Text = "label5";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(414, 226);
+            label6.Name = "label6";
+            label6.Size = new Size(50, 20);
+            label6.TabIndex = 7;
+            label6.Text = "label6";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(422, 234);
+            label7.Name = "label7";
+            label7.Size = new Size(50, 20);
+            label7.TabIndex = 8;
+            label7.Text = "label7";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(430, 242);
+            label8.Name = "label8";
+            label8.Size = new Size(50, 20);
+            label8.TabIndex = 9;
+            label8.Text = "label8";
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(506, 110);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(151, 121);
+            listView1.TabIndex = 10;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView2
+            // 
+            listView2.Location = new Point(560, 298);
+            listView2.Name = "listView2";
+            listView2.Size = new Size(151, 121);
+            listView2.TabIndex = 11;
+            listView2.UseCompatibleStateImageBehavior = false;
+            // 
             // frmConsultPedidos
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(listView2);
+            Controls.Add(listView1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button1);
             Controls.Add(label2);
             Controls.Add(textBox1);
             Controls.Add(label1);
@@ -77,5 +174,14 @@
         private Label label1;
         private TextBox textBox1;
         private Label label2;
+        private Button button1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private ListView listView1;
+        private ListView listView2;
     }
 }
