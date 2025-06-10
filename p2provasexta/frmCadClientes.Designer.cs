ed5546762c2de97c7b00ab7c1f3cafc637d9abda
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-10 16:09:07 -0300

    adicionei no frmCadClientes 12 label, 11 textBox, 4 buttons e um DataGridView

--- p2provasexta/frmCadClientes.Designer.cs
+++ p2provasexta/frmCadClientes.Designer.cs
@@ -29,25 +29,302 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(283, 27);
+            label1.Location = new Point(283, 9);
             label1.Name = "label1";
             label1.Size = new Size(145, 20);
             label1.TabIndex = 0;
             label1.Text = "Cadastro de Clientes";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(334, 169);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "Adicionar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(518, 169);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 3;
+            button3.Text = "Editar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(694, 169);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 4;
+            button4.Text = "Excluir";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(26, 182);
+            button1.Name = "button1";
+            button1.Size = new Size(208, 29);
+            button1.TabIndex = 5;
+            button1.Text = "Buscar CEP";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(334, 204);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(454, 235);
+            dataGridView1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(85, 50);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(149, 27);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(68, 83);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(166, 27);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(85, 116);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(149, 27);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(69, 149);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(165, 27);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(109, 217);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(85, 250);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(149, 27);
+            textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(71, 283);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(163, 27);
+            textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(85, 316);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(149, 27);
+            textBox8.TabIndex = 14;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(76, 349);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(158, 27);
+            textBox9.TabIndex = 15;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(88, 382);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(146, 27);
+            textBox10.TabIndex = 16;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(98, 415);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(136, 27);
+            textBox11.TabIndex = 17;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 53);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 20);
+            label2.TabIndex = 18;
+            label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 83);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 20);
+            label3.TabIndex = 19;
+            label3.Text = "CPF:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(26, 119);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 20);
+            label4.TabIndex = 20;
+            label4.Text = "E-mail:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(26, 152);
+            label5.Name = "label5";
+            label5.Size = new Size(37, 20);
+            label5.TabIndex = 21;
+            label5.Text = "CEP:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(13, 224);
+            label6.Name = "label6";
+            label6.Size = new Size(90, 20);
+            label6.TabIndex = 22;
+            label6.Text = "Logradouro:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(13, 257);
+            label7.Name = "label7";
+            label7.Size = new Size(66, 20);
+            label7.TabIndex = 23;
+            label7.Text = "Número:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(13, 290);
+            label8.Name = "label8";
+            label8.Size = new Size(52, 20);
+            label8.TabIndex = 24;
+            label8.Text = "Bairro:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(13, 323);
+            label9.Name = "label9";
+            label9.Size = new Size(59, 20);
+            label9.TabIndex = 25;
+            label9.Text = "Cidade:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(13, 352);
+            label10.Name = "label10";
+            label10.Size = new Size(57, 20);
+            label10.TabIndex = 26;
+            label10.Text = "Estado:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(13, 389);
+            label11.Name = "label11";
+            label11.Size = new Size(69, 20);
+            label11.TabIndex = 27;
+            label11.Text = "Telefone:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(13, 422);
+            label12.Name = "label12";
+            label12.Size = new Size(79, 20);
+            label12.TabIndex = 28;
+            label12.Text = "Whatsapp:";
             // 
             // frmCadClientes
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(label1);
             Name = "frmCadClientes";
             Text = "frmCadClientes";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +332,32 @@
         #endregion
 
         private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
     }
 }
