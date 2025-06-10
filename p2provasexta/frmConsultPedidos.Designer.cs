32f77636197742399764ea2db8140426236d418c
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-10 16:47:47 -0300

    organizei a interface do frmConsultPedidos

--- p2provasexta/frmConsultPedidos.Designer.cs
+++ p2provasexta/frmConsultPedidos.Designer.cs
@@ -69,80 +69,80 @@
             // 
             // button1
             // 
-            button1.Location = new Point(371, 137);
+            button1.Location = new Point(37, 110);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(167, 29);
             button1.TabIndex = 3;
-            button1.Text = "button1";
+            button1.Text = "Buscar";
             button1.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(390, 202);
+            label3.Location = new Point(37, 152);
             label3.Name = "label3";
-            label3.Size = new Size(50, 20);
+            label3.Size = new Size(53, 20);
             label3.TabIndex = 4;
-            label3.Text = "label3";
+            label3.Text = "Nome:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(398, 210);
+            label4.Location = new Point(37, 188);
             label4.Name = "label4";
-            label4.Size = new Size(50, 20);
+            label4.Size = new Size(46, 20);
             label4.TabIndex = 5;
-            label4.Text = "label4";
+            label4.Text = "Valor:";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(406, 218);
+            label5.Location = new Point(37, 226);
             label5.Name = "label5";
-            label5.Size = new Size(50, 20);
+            label5.Size = new Size(64, 20);
             label5.TabIndex = 6;
-            label5.Text = "label5";
+            label5.Text = "Pedidos:";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(414, 226);
+            label6.Location = new Point(521, 78);
             label6.Name = "label6";
-            label6.Size = new Size(50, 20);
+            label6.Size = new Size(115, 20);
             label6.TabIndex = 7;
-            label6.Text = "label6";
+            label6.Text = "Itens do Pedido:";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(422, 234);
+            label7.Location = new Point(616, 273);
             label7.Name = "label7";
-            label7.Size = new Size(50, 20);
+            label7.Size = new Size(115, 20);
             label7.TabIndex = 8;
-            label7.Text = "label7";
+            label7.Text = "total do Pedido:";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(430, 242);
+            label8.Location = new Point(616, 304);
             label8.Name = "label8";
-            label8.Size = new Size(50, 20);
+            label8.Size = new Size(83, 20);
             label8.TabIndex = 9;
-            label8.Text = "label8";
+            label8.Text = "Valor Total:";
             // 
             // listView1
             // 
-            listView1.Location = new Point(506, 110);
+            listView1.Location = new Point(37, 261);
             listView1.Name = "listView1";
-            listView1.Size = new Size(151, 121);
+            listView1.Size = new Size(280, 121);
             listView1.TabIndex = 10;
             listView1.UseCompatibleStateImageBehavior = false;
             // 
             // listView2
             // 
-            listView2.Location = new Point(560, 298);
+            listView2.Location = new Point(521, 110);
             listView2.Name = "listView2";
-            listView2.Size = new Size(151, 121);
+            listView2.Size = new Size(242, 121);
             listView2.TabIndex = 11;
             listView2.UseCompatibleStateImageBehavior = false;
             // 
