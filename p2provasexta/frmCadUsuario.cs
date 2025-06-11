1d9108d593d13aff8a1fd3d66432b4c94844c10a
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-11 16:30:29 -0300

    apaguei tudo que tinha programado no frmCadUsuario por conta de não identificar o arquivo csv, estava dando interferencia no projeto e não consegui resolver nem com auxilio de colegas e nem IA

--- p2provasexta/frmCadUsuario.cs
+++ p2provasexta/frmCadUsuario.cs
@@ -24,60 +24,17 @@ namespace p2provasexta
 
         private void AtualizarLista()
         {
-            listBox1.Items.Clear();
-            var linhas = File.ReadAllLines("usuario.csv").Skip(1);
-            foreach (var line in linhas)
-            {
-                var partes = line.Split(',');
-                listBox1.Items.Add(partes[0]);
-            }
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            string usuario = textBox1.Text.Trim();
-            string senha = textBox2.Text.Trim();
-
-            if (usuario == "" || senha == "")
-            {
-                MessageBox.Show("Preencha os campos.");
-                return;
-            }
-
-            var linhas = File.ReadAllLines("usuarios.csv").ToList();
-            if (linhas.Any(l => l.Split(',')[0] == usuario))
-            {
-                MessageBox.Show("usuario já existe.");
-                return;
-            }
-
-            linhas.Add($"{usuario},{senha}");
-            File.WriteAllLines("usuarios.csv", linhas);
-            MessageBox.Show("Usuário adicionado!");
-            AtualizarLista();
+        {     
+            
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (listBox1.SelectedItem ==  null)
-            {
-                MessageBox.Show("Selecione um usuário.");
-                return;
-            }
 
-            string selecionado = listBox1.SelectedItem.ToString();
-            if (selecionado == "ADMIN")
-            {
-                MessageBox.Show("Não é permitido excluir o ADMIN.");
-                return;
-            }
-
-            var linhas = File.ReadAllLines("usuarios.csv").ToList();
-            linhas = linhas.Where(l => l.Split(',')[0] != selecionado).ToList();
-            File.WriteAllLines("usuarios.csv", linhas);
-
-            MessageBox.Show("Usuário excluido!");
-            AtualizarLista();
         }
     }
 }
