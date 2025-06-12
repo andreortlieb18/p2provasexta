4af3bdcdd7841c3034bec3f7a5dd215e97555615
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-11 21:14:53 -0300

    adicionei a função de apagar os dados do dataGridView1 do frmCadProdutos através de um button "excluir"

--- p2provasexta/frmCadProdutos.Designer.cs
+++ p2provasexta/frmCadProdutos.Designer.cs
@@ -102,7 +102,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(322, 241);
+            button1.Location = new Point(322, 238);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 7;
