f678d8c2fe081eac2bbe3cf9435539572e2dba60
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-11 22:32:29 -0300

    Não consegui desenvolver o projeto da forma que foi proposta, mas o que eu consegui fazer e o que eu sei fazer, ta feito

--- p2provasexta/frmCadProdutos.Designer.cs
+++ p2provasexta/frmCadProdutos.Designer.cs
@@ -81,7 +81,7 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(72, 145);
+            textBox1.Location = new Point(72, 152);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(125, 27);
             textBox1.TabIndex = 4;
